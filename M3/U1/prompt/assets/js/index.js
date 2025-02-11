ed76303565a4259459b4c62d7940004eda77e23c
tablonimus <lautamartin1995@gmail.com>
2025-02-10 21:01:59 -0300

    JS

---
+++ M3/U1/prompt/assets/js/index.js
@@ -0,0 +1,13 @@
+/* PROMPT=> almacena en una variable una cadena de caracteres con lo que le escriba. */
+// let nombre = prompt("Ingrese su nombre de pila:");
+// let apellido = prompt("Ingrese su apellido de pila:");
+// let edad = prompt("Ingrese su edad:");
+// alert("Bienvenido " + nombre + " " + apellido + " tu edad es de: " + edad);
+
+
+let num1 = prompt("Ingrese un número")//"10" => 10
+let num2 = prompt("Ingrese otro número")//"10" => 10
+
+let resultado = Number(num1) + Number(num2) //=> Number convierte el dato a Número
+
+alert("su resultado es=>" + resultado)
